e816a7ab0be430619993f27265cca104bac1f2a7
mtlankenau <mtlankenau@gmail.com>
2022-01-18 15:34:35 -0500

    updated test files to include updates to Nav and new Contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -35,6 +35,7 @@ function ContactForm() {
 
   return (
     <section>
+      <h1 data-testid="h1tag">Contact Me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
@@ -53,7 +54,7 @@ function ContactForm() {
             </div>
           )}
         </div>
-        <button type="submit">Submit</button>
+        <button data-testid="buttontag" type="submit">Submit</button>
       </form>
     </section>
   )
